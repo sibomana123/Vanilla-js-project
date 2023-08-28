c4bb15b043689870c90b88de5ba39630c5d25f23
Emmanuel <emmanuelsibomana69@gmail.com>
2023-08-28 10:18:07 +0200

    Modified hex.js

--- 1Color-Flipper/hex.js
+++ 1Color-Flipper/hex.js
@@ -2,17 +2,23 @@ const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 
-btn.addEventListener("click", function () {
-  let hexColor = "#";
-  for (let i = 0; i < 6; i++) {
-    hexColor += hex[getRandomNumber()];
+btn.addEventListener('click', () => {
+  let HexColor = '#'
+  for (let i = 0; i < 6; i++){
+    // you can call randomNumber as function referance or like this
+
+    let randomNumber = Math.floor(Math.random() * hex.length);
+
+    HexColor += hex[randomNumber];
+    // console.log(HexColor);
   }
-  // console.log(hexColor);
 
-  color.textContent = hexColor;
-  document.body.style.backgroundColor = hexColor;
-});
+  color.textContent = HexColor;
+  document.body.style.backgroundColor = HexColor;
+})
 
- const getRandomNumber =()=>{
-  return Math.floor(Math.random() * hex.length);
-}
+
+
+// function randomNumber() {
+//  return Math.floor(Math.random()* hex.length)
+// }
