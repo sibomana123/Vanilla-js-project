7d5bc4a53fd6026c76d36eccabb9a7fd3d133386
Emmanuel <emmanuelsibomana69@gmail.com>
2023-08-31 11:30:22 +0200

    video project

---
+++ 8video-project/emmy.js
@@ -0,0 +1,32 @@
+
+
+const btn = document.querySelector('.switch-btn')
+const video = document.querySelector(".video-container");
+
+
+
+btn.addEventListener('click', () => {
+  console.log('clicked');
+
+  if (!btn.classList.contains('slide')) {
+    btn.classList.add('slide')
+    video.pause();
+  } else {
+    btn.classList.remove('slide')
+    video.play();
+  }
+})
+
+
+// preloader
+
+const preloader = document.querySelector(".preloader");
+
+window.addEventListener('load', () => {
+  preloader.classList.add("hide-preloader");
+
+  // console.log('loded');
+})
+
+
+ 
